a0847fbd9a23fd5dc129a9c83635ef2ed78fdf1e
MatthewKosloski <matthewkosloski@me.com>
2017-03-08 23:09:24 -0600

    added assignments.  continue on page 365

---
+++ chapter/seven/Season.java
@@ -0,0 +1,25 @@
+//********************************************************************
+// Season.java
+//
+// Enumerates the values for Season.
+//********************************************************************
+
+public enum Season {
+	winter ("December through February"),
+	spring ("March through May"),
+	summer ("June through August"),
+	fall ("September through November");
+
+	private String span;
+
+	// Constructor: Sets up each value with an associated string.
+	Season(String months) {
+		span = months;
+	}
+
+	// Returns the span message for this value.
+	public String getSpan() {
+		return span;
+	}
+
+}
